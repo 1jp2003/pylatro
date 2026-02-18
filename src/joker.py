2e08b947c7c0ff78b354ae72a3cd52aa66f08d3c
1jp2003 <69287271+1jp2003@users.noreply.github.com>
2026-02-17 18:07:55 -0700

    add hooks for jokers

--- src/joker.py
+++ src/joker.py
@@ -17,7 +17,8 @@ class Joker:
         self.edition = edition
         self.sticker = sticker
         # The internal value for scaling Jokers
-        self.current_bonus = 0 
+        self.current_chip_bonus = 0 
+        self.current_mult_bonus = 0 
         # For 'perishable' or 'probability' Jokers (like Gros Michel)
         self.is_active = True 
         # For 'eternal' stickers
@@ -27,23 +28,25 @@ class Joker:
     
     # when a card is triggered / played in hand 
     # photo, chad, hiker, etc 
-    def on_card_scored(self):
-        return
+    def on_card_scored(self, card, hand_name, is_played):
+        return 0, 0, 1.0
     
     # when the full hand is selected
     # before on_card_scored
     # vampire, midas, duo, trio? 
-    def on_hand_scored(self):
-        return
+    def on_hand_scored(self, hand_name, current_chips, current_mult):
+        return 0, 0, 1.0
     
     # when the round is over (before cash out)
     # Mime, golden joker, 
-    def on_blind_end(self):
+    def on_blind_end(self, gamestate):
         return
     
+    def post_hand(self, cards_played, gamestate):
+        return
     # When the round starts
     # cartomancer, marble joker
-    def on_blind_begin(self):
+    def on_blind_begin(self, gamestate):
         return
     
     # When a pack is opened
@@ -52,17 +55,20 @@ class Joker:
         return
     
     # red card???
-    def on_pack_close(self):
+    def on_pack_skip(self):
         return
     
     # Trading card, yorick 
     def on_card_discard(self):
         return
     
+    # Invis / cola
     def on_sell(self):
+        # add self.sell_value to money and check joker conditions
         return
     
-    def get_retriggers(self):
+    # should deal with retriggers played 
+    def get_retriggers(self, card, hand_name, is_played):
         return 0
     
     def get_edition_bonus(self):
